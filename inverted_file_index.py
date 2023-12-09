480b7dfd59b5bd0f9cafa944ad6c6dc8d67e63d7
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-12-09 23:27:41 +0200

    retrive fucn for the inverted file index

--- inverted_file_index.py
+++ inverted_file_index.py
@@ -4,6 +4,7 @@ from math import log2
 from random import random
 from math import ceil
 from sklearn.cluster import KMeans
+import heapq
 
 # in this file we will implement a better version of VecDB
 # we have to use better indexing method to speed up the retrival process
@@ -50,8 +51,49 @@ class VecDBIF:
 
     # TODO: change this function to retreive from the indexed Inverted file index db
     def retrive(self, query: Annotated[List[float], 70], top_k = 5):
-        return [int(random() * 1000) for _ in range(top_k)]
-        pass
+        # now we need to find the n closest centroids to the query
+        # we will use the kmeans model to find the closest centroids
+        # gen n centroids where n is a hyperparameter
+        n = 10 # number of nearest centroids to get
+        nearest_centroids = sorted(self.kmeans.cluster_centers_, key=lambda centroid: self._cal_score(query, centroid), reverse=True)[:n]
+        # now we need to get the label of each centroid
+        nearest_centroids = [self.kmeans.predict([centroid])[0] for centroid in nearest_centroids]
+        print("nearest_centroids", nearest_centroids)
+
+        # now we need to search in the files of the nearest centroids
+        # we will get the top k vectors from each file
+        # then we will sort them by their score
+        # then we will return the top k vectors
+        # we will use a heap to do that cause it is faster
+        heap = [] 
+        heapq.heapify(heap)
+        for centroid in nearest_centroids:
+            # open the file of the centroid
+            f = open(f"cluster_{centroid}.csv", "r")
+            # read the file line by line
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                # split the line to get the id and the vector
+                # the first number will be the id
+                # the rest of the numbers will be the vector
+                id = int(line.split(",")[0])
+                vect = [float(e) for e in line.split(",")[1:]]
+                # calculate the score of the vector
+                score = self._cal_score(query, vect)
+                # add it to the heap
+                heapq.heappush(heap, (-score, id, vect))
+            f.close()
+        # now we have the top k vectors in the heap
+        # we will pop them from the heap and return them
+        ids_scores = []
+        for _ in range(top_k):
+            score, id, vect = heapq.heappop(heap)
+            ids_scores.append(id)
+
+        return ids_scores
+
     
     
 
